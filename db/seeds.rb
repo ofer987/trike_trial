795599942161c4772b96a4100692a8144fca5a24
Dan Galipo <dan.v.galipo@gmail.com>
2015-03-16 13:58:54 +1100

    Fixed country code typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 Country.create(:name => "Australia", :code => "AU", :regular_shipping_rate => "20.00", :express_shipping_rate => "50.00")
-Country.create(:name => "United States", :code => "AU", :regular_shipping_rate => "40.00", :express_shipping_rate => "60.00")
+Country.create(:name => "United States", :code => "US", :regular_shipping_rate => "40.00", :express_shipping_rate => "60.00")
