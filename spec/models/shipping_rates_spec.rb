959fdc28a51586a934f63d8e8e9d56f7e787bced
Dan Jakob Ofer <ofer987@gmail.com>
2016-12-20 00:04:11 +0000

    More comprehensive specs

--- spec/models/shipping_rates_spec.rb
+++ spec/models/shipping_rates_spec.rb
@@ -1,34 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe ShippingRates do
-  let(:country) { nil }
-  subject { described_class.new(currency) }
-
   context 'for a given country' do
-    let(:country) { FactoryGirl.create(:country) }
-    let(:currency) do
+    let!(:country) { FactoryGirl.create(:country) }
+    let!(:currency) do
       FactoryGirl.create(:currency,
                          country: country,
                          regular_shipping_rate: '10.00',
                          express_shipping_rate: '20.00')
     end
 
+    subject { described_class.new(currency) }
+
     its(:regular) { should eq('10.00') }
     its(:express) { should eq('20.00') }
     its(:as_json) { should eq( { regular: '10.00', express: '20.00' } ) }
-  end
 
-  context 'for another country' do
-    let(:country) { FactoryGirl.create(:country) }
-    let(:currency) do
-      FactoryGirl.create(:currency,
-                         country: country,
-                         regular_shipping_rate: '5.00',
-                         express_shipping_rate: '8.00')
+    context 'for another country' do
+      let!(:another_country) { FactoryGirl.create(:country) }
+      let!(:another_currency) do
+        FactoryGirl.create(:currency,
+                           country: another_country,
+                           regular_shipping_rate: '5.00',
+                           express_shipping_rate: '8.00')
+      end
+
+      subject { described_class.new(another_currency) }
+
+      its(:regular) { should eq('5.00') }
+      its(:express) { should eq('8.00') }
+      its(:as_json) { should eq( { regular: '5.00', express: '8.00' } ) }
+
+      context 'for another currency' do
+        let!(:funny_money) do
+          FactoryGirl.create(:currency,
+                             country: another_country,
+                             regular_shipping_rate: '125.25',
+                             express_shipping_rate: '599.99')
+        end
+
+        subject { described_class.new(funny_money) }
+
+        its(:regular) { should eq('125.25') }
+        its(:express) { should eq('599.99') }
+
+        its(:as_json) { should eq( { regular: '125.25', express: '599.99' } ) }
+      end
     end
-
-    its(:regular) { should eq('5.00') }
-    its(:express) { should eq('8.00') }
-    its(:as_json) { should eq( { regular: '5.00', express: '8.00' } ) }
   end
 end
